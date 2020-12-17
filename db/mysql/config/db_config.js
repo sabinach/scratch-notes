0ba8cc18e154fbb061e396025c609de8aab9e2e6
Sabina Chen <sabinach@mit.edu>
2020-12-17 17:20:53 -0500

    added localhost values

--- db/mysql/config/db_config.js
+++ db/mysql/config/db_config.js
@@ -1,6 +1,6 @@
 module.exports = {
   HOST: "us-cdbr-east-02.cleardb.com", // localhost
-  USER: "b7b60d1723d268",
-  PASSWORD: "541c325d",
-  DB: "heroku_7d5061ec7f81f43",
+  USER: "b7b60d1723d268", // "root"
+  PASSWORD: "541c325d", // "1234"
+  DB: "heroku_7d5061ec7f81f43", // "testdb"
 };
