ef9449f8ea8b952aca990d1497ee8e04016972bc
Sabina Chen <sabinach@mit.edu>
2020-12-17 04:25:34 -0500

    removed .env

--- db/mysql/db/db_config.js
+++ db/mysql/db/db_config.js
@@ -1,8 +1,6 @@
-require("dotenv").config();
-
 module.exports = {
-  HOST: process.env.DB_USERNAME, // localhost
-  USER: process.env.DB_PASSWORD,
-  PASSWORD: process.env.DB_HOST,
-  DB: process.env.DB_DATABASE,
+  HOST: "us-cdbr-east-02.cleardb.com", // localhost
+  USER: "b7b60d1723d268",
+  PASSWORD: "541c325d",
+  DB: "heroku_7d5061ec7f81f43",
 };
