0ee4393ead84133d379b4cb359055e090358257a
Sabina Chen <15172577+sabinach@users.noreply.github.com>
2022-01-30 03:54:59 -0500

    added exports

--- discord/testBot/utils.js
+++ discord/testBot/utils.js
@@ -1,38 +1,38 @@
-const getRoleFromId = (guild, roleId) =>
+export const getRoleFromId = (guild, roleId) =>
   guild.roles.cache.get(roleId)
 
-const getRoleFromName = (guild, roleName) =>
+export const getRoleFromName = (guild, roleName) =>
   guild.roles.cache.find(r => r.name === roleName)
 
-const getUserFromId = (guild, userId) =>
+export const getUserFromId = (guild, userId) =>
   guild.members.cache.get(userId)
 
-const getUserFromName = (guild, userName) =>
+export const getUserFromName = (guild, userName) =>
   guild.members.cache.find(u => u.name === userName)
 
-const isRoleExist = (guild, roleName) =>
+export const isRoleExist = (guild, roleName) =>
   getRoleFromName(guild, roleName) !== undefined
 
-const isRoleIdExist = (guild, roleId) =>
+export const isRoleIdExist = (guild, roleId) =>
   getRoleFromId(guild, roleId) !== undefined
 
-const isUserExist = (guild, userName) =>
+export const isUserExist = (guild, userName) =>
   getUserFromName(guild, userName) !== undefined
 
-const isUserIdExist = (guild, userId) =>
+export const isUserIdExist = (guild, userId) =>
   getUserFromId(guild, userId) !== undefined
 
-const hasRole = (member, roleName) =>
+export const hasRole = (member, roleName) =>
   member.roles.cache.some(r => r.name === roleName)
 
-const hasRoleId = (member, roleId) =>
+export const hasRoleId = (member, roleId) =>
   member.roles.cache.has(roleId)
 
-const hasPerm = (member, permId) =>
+export const hasPerm = (member, permId) =>
   member.permissions.has(permId)
 
-const createRoleMention = (roleId) =>
+export const createRoleMention = (roleId) =>
   `<@&${roleId}>`
 
-const createRoleMention = (userId) =>
+export const createRoleMention = (userId) =>
   `<@${userId}>`
